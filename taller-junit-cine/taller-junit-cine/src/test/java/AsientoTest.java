d309df301d3bc0a98159addab17add919c5ac261
colorboss1 <danegret@espol.edu.ec>
2026-07-22 14:30:42 -0500

    Update AsientoTest.java
    commit 1 n

--- taller-junit-cine/taller-junit-cine/src/test/java/AsientoTest.java
+++ taller-junit-cine/taller-junit-cine/src/test/java/AsientoTest.java
@@ -1,5 +1,10 @@
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class AsientoTest {
     Asiento asiento;
